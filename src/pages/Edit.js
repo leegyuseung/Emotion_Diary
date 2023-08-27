e374bba076cca8a345ce1b8b10a1d806f8886ba8
lee gyuseung <gyus0421@gmail.com>
2023-08-27 23:39:54 +0900

    [Feat💡] React Router 기본
    1. Path Variable => useParams
    2. Query String => useSearchParams
    3. Page Moving => useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  // Page Moving
+  const navigate = useNavigate();
+
+  // QueryString
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id:", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode:", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      {/* QueryString을 바꾸는 상태변화함수 */}
+      <button onClick={() => setSearchParams({ who: "james" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
