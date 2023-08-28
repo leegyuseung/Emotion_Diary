98916fa9b5281c649cdb178ccdab7bc4a9136873
lee gyuseung <gyus0421@gmail.com>
2023-08-28 23:42:08 +0900

    [Feat💡] 프로젝트 기초 공사
    공통컴포넌트 MyHeader 세팅하기

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     // BrowserRouter로 감싸자있으면 브라우저 라우터에 맵핑될수 있다는 뜻
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText="App"
+          leftChild={
+            <MyButton
+              text={"왼쪽버튼"}
+              onClick={() => {
+                alert("왼쪽 클릭");
+              }}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽버튼"}
+              onClick={() => {
+                alert("오른쪽 클릭");
+              }}
+            />
+          }
+        />
         <h2>App.js</h2> {/* Route 바깥 쪽이라 모든페이지에서 볼 수 있다. */}
         <MyButton
           text={"버튼"}
