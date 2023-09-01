aeb8da07b8a456f46511a8b47b0f7ea87ae8cc9c
lee gyuseung <gyus0421@gmail.com>
2023-09-01 22:55:23 +0900

    [Feat💡] 프로젝트 기초 공사 2
    useReducer를 이용해 기초공사하기

--- src/App.js
+++ src/App.js
@@ -5,54 +5,68 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
+import { useReducer, useRef } from "react";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE":
+      newState = [action.data, ...state];
+      break;
+    case "REMOVE":
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    case "EDIT":
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    default:
+      return state;
+  }
+  return newState;
+};
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
     // BrowserRouter로 감싸자있으면 브라우저 라우터에 맵핑될수 있다는 뜻
     <BrowserRouter>
       <div className="App">
-        <MyHeader
-          headText="App"
-          leftChild={
-            <MyButton
-              text={"왼쪽버튼"}
-              onClick={() => {
-                alert("왼쪽 클릭");
-              }}
-            />
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽버튼"}
-              onClick={() => {
-                alert("오른쪽 클릭");
-              }}
-            />
-          }
-        />
         <h2>App.js</h2> {/* Route 바깥 쪽이라 모든페이지에서 볼 수 있다. */}
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-          type={"negative"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-        />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/new" element={<New />}></Route>
