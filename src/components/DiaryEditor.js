cc945fb69a49e249fe524517914a20a1d801af54
lee gyuseung <gyus0421@gmail.com>
2023-09-10 00:13:27 +0900

    [Feat💡] 페이지구현 - 일기 쓰기(/new)
    DiaryEditor의 두 번째 섹션 구현하기 (오늘은 감정)

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -3,6 +3,35 @@ import { useNavigate } from "react-router-dom";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "끔찍함",
+  },
+];
 
 // 오늘 날짜를 형식을 바꿔주기 위함
 const getStringDate = (date) => {
@@ -11,7 +40,12 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
 
   const navigate = useNavigate();
 
@@ -33,6 +67,19 @@ const DiaryEditor = () => {
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
           </div>
         </section>
       </div>
